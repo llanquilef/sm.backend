46b5d264a745301279bd5de42b0ceb5e65772d4e
llanquilef <llanquilef.cl@gmail.com>
2026-01-31 13:48:09 -0300

    Se crean nuevas clases para mantener una logica mas ordenada

--- backend/models/subscription.py
+++ backend/models/subscription.py
@@ -1,6 +1,6 @@
 from sqlmodel import Field, SQLModel
 from datetime import datetime
-from models.company import CompanyBase as BaseCompany
+from models.company import CompanyBase
 from models.plan import PlanBase
 from utils.timestamps.DateTimeToUTC import get_time_to_utc
 import uuid
@@ -11,23 +11,35 @@ def __init__(self, subscription: dict):
 
 STATUS = ['inactive', 'active', 'testing']
  
-class SubscriptionBase(SQLModel): 
-    super.__init__()
+class SubscriptionBase(SQLModel):
+    """ 
+    Clase SubscriptionBase: Esta clase nos permite el obtener los atributos principales de la clase
+    Class SubscriptionBase: This class allows to get all the principal attributes de la clase 
+    """
     id: uuid.UUID = Field(default_factory= uuid.uuid4, primary_key=True)
-    companyId: int = Field(foreign_key=BaseCompany.id)
-    planId: int = Field(foreign_key=PlanBase.id)
     
+class SubscriptionFK(SubscriptionBase): 
+    super.__init__() 
+    companyId: uuid.UUID = Field(foreign_key=CompanyBase.id)
+    planId: uuid.UUID = Field(foreign_key=PlanBase.id)
+         
 class SubscritionCreate(SubscriptionBase):
-    super.__init__()
+    super.__init__()    
     ds: datetime | None = Field(default_factory=get_time_to_utc, nullable=False)
     de: datetime | None = Field(default_factory=get_time_to_utc, nullable=False)
 
-class SubscriptionRead(SubscriptionBase and SubscritionCreate):
+class SubscriptionDelete(SubscriptionBase):
+    super.__init__()
+    deletedAt: datetime = Field(default_factory=get_time_to_utc)
+    
+
+class SubscriptionRead(SubscriptionBase):
     super.__init__()
     status: str = Field(STATUS, default=STATUS[1]) 
-    pass
-
+    
 class Subscription(SubscriptionRead):
     super.__int__()
-    pass 
+    pass
+
+
      
